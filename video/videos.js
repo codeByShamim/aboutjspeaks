10b4404a25bc90965851e89e84d7c88388be440a
Shamim Ahmed <shamimbro@outlook.com>
2024-11-23 21:42:50 +0600

    Initial commit

--- video/videos.js
+++ video/videos.js
@@ -1,9 +1,9 @@
 // Wait for the page to load, then play the video after a 2-second delay
-window.onload = function() {
+window.onload = function () {
     setTimeout(() => {
         // Hide the thumbnail image
         document.getElementById('videoThumbnail').style.display = 'none';
-        
+
         // Show the local video and play it
         const localVideo = document.getElementById('localVideo');
         localVideo.style.display = 'block';
